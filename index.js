f142e6c71700dbb7028eb62577d7f8d5e93aebbe
Johhny Bravo <arjuns062009@gmail.com>
2017-12-03 19:01:18 +0530

    second

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var id = "dev1";
-var pass = "pass";
+var methods = require('./methods_here');
+var id = methods.id;
+var pass = methods.pass;
 var callString = `https://ocodeioe.herokuapp.com/?id=${id}&pass=${pass}`;
 var socket = require("socket.io-client")(callString);
-function doFunction(args){
-    console.log(args);
-}
 socket.on(id+pass, (data) => {
-    doFunction(data);
+    methods.input(data);
 });
---
+++ methods_here.js
@@ -0,0 +1,24 @@
+//define your id and password here
+var id = "dev1";
+var pass = "pass";
+
+//start coding here
+var Gpio = require('onoff').Gpio;
+var LED = new Gpio(4, 'out');
+
+//default function, let it stay. data contains the input from the web socket
+function input(data){
+    console.log(data);
+    if(data.led == 0){
+        LED.writeSync(0);
+    }
+    else{
+        LED.writeSync(1);
+    }
+}
+
+
+//do not touch these
+module.exports.input = input;
+module.exports.id = id;
+module.exports.pass = pass;
