ee87c41d2d8d966c1653862fa36a692c117ad064
Arjun Kashyap <arjuns062009@gmail.com>
2017-12-04 09:57:44 +0530

    minor update

--- methods_here.js
+++ methods_here.js
@@ -3,18 +3,19 @@ var id = "dev1";
 var pass = "pass";
 
 //start coding here
-var Gpio = require('onoff').Gpio;
-var LED = new Gpio(4, 'out');
+//var Gpio = require('onoff').Gpio;
+//var LED = new Gpio(4, 'out');
 
 //default function, let it stay. data contains the input from the web socket
 function input(data){
-    console.log(data);
+    /*console.log(data);
     if(data.led == 0){
         LED.writeSync(0);
     }
     else{
         LED.writeSync(1);
-    }
+    }*/
+    console.log(data);
 }
 
 
